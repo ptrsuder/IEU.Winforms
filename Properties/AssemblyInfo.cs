516cc8f2c7f7af99f24ad5c2fff62b05d458f9de
ptrsuder <ptrsuder@gmail.com>
2019-07-23 21:54:47 +0300

    Remove VS-generated comments

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
 [assembly: AssemblyTitle("Image Enhancing Utility")]
 [assembly: AssemblyDescription("GUI tool for ESRGAN")]
 [assembly: AssemblyConfiguration("")]
@@ -13,24 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © ptrsuder 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("211ce32d-0337-4bdb-858e-3b87bb4a833e")]
-
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//   Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.02.0")]
 [assembly: AssemblyFileVersion("0.9.02.0")]
